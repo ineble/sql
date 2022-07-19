b4df3d6f0f9b070f78499d7fdd6e111427e4f2b1
ineble <thdwltjq94@gmail.com>
2022-07-19 14:16:14 +0900

    login multi-version

--- pro06/src/main/java/sec01/ex01/InputServlet2.java
+++ pro06/src/main/java/sec01/ex01/InputServlet2.java
@@ -23,9 +23,15 @@ public class InputServlet2 extends HttpServlet {
 	throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		Enumeration enu = req.getParameterNames();
+		// 열거타입으로 Names를 받아서 enu에 넣어라
+		// 파라미터의 이름 집합을 Enumerration 객체로 반환하는 메소드
+		// 지금은 정해진 매개변수가 없으므로 전체를 가져온다.
 		while(enu.hasMoreElements()) {
+			//더 엘리먼트가 있냐?-> 있으면 true 없으면 false반환
 			String name = (String) enu.nextElement();
+			// String으로 형변환 하고 enu에 다음 엘리멘트의 name에 넣어라 
 			String[] values = req.getParameterValues(name);
+			// name의 Values를 String[] values로 넣는다.
 			for(String value: values) {
 				System.out.println("name= " + name + ",value= " + value);
 			}
